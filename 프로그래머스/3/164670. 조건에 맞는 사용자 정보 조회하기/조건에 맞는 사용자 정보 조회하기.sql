8df6f0ba61cb63c0fb8c8689409c270d71e64bf8
Han Jaeyoon <dkxmpower999@naver.com>
2025-01-19 22:03:44 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,20 @@
+-- 코드를 입력하세요
+SELECT 
+    UGU.USER_ID,
+    UGU.NICKNAME,
+    UGU.CITY || ' ' ||UGU.STREET_ADDRESS1 || ' ' || UGU.STREET_ADDRESS2 AS "전체주소",
+    CASE WHEN LENGTH(TLNO) = 11 
+         THEN SUBSTR(TLNO,0,3) || '-' 
+           || SUBSTR(TLNO,4,4) || '-' 
+           || SUBSTR(TLNO,8,4) 
+    END AS "전화번호"
+FROM USED_GOODS_USER UGU
+WHERE USER_ID IN (
+                    SELECT WRITER_ID 
+                    FROM USED_GOODS_BOARD UGB
+                    GROUP BY WRITER_ID
+                    HAVING COUNT(BOARD_ID) >= 3
+                  )
+ORDER BY USER_ID DESC
+;
+                
